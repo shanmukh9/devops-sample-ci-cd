aa87f93085b050f4b598a3f5c847373a5da6e126
Shanmukh <yalavarthisainadh@gmail.com>
2022-09-11 07:30:48 -0400

    added deployment.yaml & service.yaml files

--- app.py
+++ app.py
@@ -1,13 +1,11 @@
 from flask import Flask
 import os
- 
 app = Flask(__name__)
- 
- 
+
 @app.route("/")
 def hello():
    return "Hello world!!! from Shanmukh"
- 
+
  
 if __name__ == "__main__":
    port = int(os.environ.get("PORT", 5000))
--- test.py
+++ test.py
@@ -1,6 +1,4 @@
 from app import app
- 
- 
 def test_hello():
    response = app.test_client().get('/')
    assert response.status_code == 200
