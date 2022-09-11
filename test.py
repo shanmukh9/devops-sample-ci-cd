9b2d130ff49026fd28a021b6e8c99db2348eb4df
YSS <yalavarthisainadh@gmail.com>
2022-09-11 15:53:45 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 from app import app
- 
- 
+
+
 def test_hello():
-  response = app.test_client().get('/')
-  assert response.status_code == 200
-  assert response.data == b'Hello world!!! from Shanmukh'
+    response = app.test_client().get('/')
+    assert response.status_code == 200
+    assert response.data == b'Hello world!!! from Shanmukh'
